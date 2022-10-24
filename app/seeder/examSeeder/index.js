ae333b8b9b8700b435b35e619c50f4a4765f6604
ivanignatius <ivanignatius98@gmail.com>
2022-10-24 16:28:23 +0700

    reformat seeder

--- app/seeder/examSeeder/index.js
+++ app/seeder/examSeeder/index.js
@@ -1,5 +1,6 @@
 const Exam = require("../../models/exam")
 const mongoose = require("mongoose")
+const questions = require("./questions_2.json")
 require('dotenv').config()
 const randomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,37 +15,67 @@ const run = async () => {
         console.log("MONGO CONNECTION ERROR : ", errMongo)
       }
     })
-  await Exam.deleteMany({})
-  let exam = []
-  while (exam.length <= 5) {
-    let questions = []
-    let qcount = exam.length == 0 ? 10 : 20
-    for (let i = 0; i < qcount; i++) {
-      const options = [
-        { "value": "Option A" },
-        { "value": "Option B" },
-        { "value": "Option C" },
-        { "value": "Option D" }
-      ]
-      questions = [...questions, {
-        title: `Question ${i + 1}`,
-        type: "MULTIPLE_CHOICE",
-        correct_answer: options[randomInteger(0, options.length - 1)].value,
-        options
-      }]
-    }
-    exam = [...exam, {
-      title: `Tryout ${exam.length + 1}`,
-      description: `Tryout ${exam.length + 1} Description`,
-      type: "REGULAR",
-      thumbnail: "",
-      duration: 120,
-      membership: "GOLD",
-      questions
-    }]
+
+  const exam = {
+    title: `ILMU BEDAH #2`,
+    description: `SOAL PREDIKSI BATCH 3 2020 #2`,
+    type: "REGULAR",
+    category: "Anestesi",
+    thumbnail: "",
+    duration: 120,
+    membership: "GOLD"
   }
 
-  await Exam.insertMany(exam)
+  // let parsedQ = []
+  // for (let { desc, correct_answer } of questions) {
+  //   let arr = desc.split("a.")
+  //   let opt = arr[1].trim()
+  //     .replace('b.', 'a.')
+  //     .replace('c.', 'a.')
+  //     .replace('d.', 'a.')
+  //     .replace('e.', 'a.')
+  //     .split("a.")
+  //   opt = opt.map(s => s.replace('\n', '').trim())
+  //   parsedQ = [...parsedQ, {
+  //     description: arr[0],
+  //     options: opt,
+  //     correct_answer,
+  //   }]
+  // }
+
+  // exam.questions = parsedQ
+
+  exam.questions = questions
+  // await Exam.deleteMany({})
+  // let exam = []
+  // while (exam.length <=5) {
+  //   let questions = []
+  //   let qcount = exam.length == 0 ? 10 : 20
+  //   for (let i = 0; i < qcount; i++) {
+  //     const options = [
+  //       { "value": "Option A" },
+  //       { "value": "Option B" },
+  //       { "value": "Option C" },
+  //       { "value": "Option D" }
+  //     ]
+  //     questions = [...questions, {
+  //       title: `Question ${i + 1}`,
+  //       type: "MULTIPLE_CHOICE",
+  //       correct_answer: options[randomInteger(0, options.length - 1)].value,
+  //       options
+  //     }]
+  //   }
+  //   exam = [...exam, {
+  //     title: `Tryout ${exam.length + 1}`,
+  //     description: `Tryout ${exam.length + 1} Description`,
+  //     type: "REGULAR",
+  //     thumbnail: "",
+  //     duration: 120,
+  //     membership: "GOLD",
+  //     questions
+  //   }]
+  // }
+  await Exam.insertMany([exam])
   console.log("done")
 }
 
---
+++ app/seeder/examSeeder/questions_2.js
@@ -0,0 +1,291 @@
+module.exports = [
+  {
+    "desc": `Anak usia 4 bulan diantar orang tuanya dengan keluhan benjolan
+    pada kantung kemaluan sejak lahir di sebelah kiri. Benjolan tidak
+    nyeri, lunak. Pemeriksaan skrotum kiri lebih besar dari kanan.
+    Demam (-), RR dan nadi normal. Pemeriksaan lanjutan yang
+    diperlukan?
+    a. Kehr sign
+    b. Finger test
+    c. Transluminasi
+    d. Digital rectal examination
+    e. Phren test`,
+    "correct_answer": "Transluminasi"
+  },
+  {
+    desc: `Laki-laki 20 tahun, setelah kecelakaan saat memakai motor
+    merasa perut bagian bawah sakit. Pasien ingin BAK namun tidak
+    bisa. Pemeriksaan fisik pada pubis kiri merah memar, scrotum
+    memar dan uretra keluar darah. Tindakan yang paling tepat
+    dilakukan pada kasus ini adalah?
+    a. Pasang kateter
+    b. Pungsi suprapubik
+    c. Hentikan perdarahan
+    d. Ureterogram
+    e. Observasi`,
+    correct_answer: "Pungsi suprapubik"
+  },
+  {
+    desc: `Pria 28 tahun datang ke IGD dengan keluhan nyeri lengan kanan
+    bawah dan riwayat terjatuh 2 jam yang lalu. Pemeriksaan fisik
+    didapatkan tanda vital dalam batas normal dan pemeriksaan
+    radiologis ditemukan fraktur 1/3 distal os radius dengan angulasi
+    ke arah palmar/ventral. Diagnosis paling memungkinkan?
+    a. Fr. Colles
+    b. Fr. Smith
+    c. Fr. Montegia
+    d. Fr. Galleazi
+    e. Fr. Greenstick`,
+    correct_answer: "Fr. Smith"
+  },
+  {
+    desc: `Pria 35 tahun datang ke IGD dengan keluhan sesak mendadak.
+    Pasien merupakan seorang perokok lebih dari 10 tahun. Tanda
+    vital TD 110/70 mmHg, nadi 100 x/min, nafas 42 x/min, T 37o C.
+    Pada pemeriksaan paru didapatkan pergerakan paru kanan
+    tertinggal dan perkusi hipersonor. Apakah tatalaksana yang tepat
+    pada pasien di atas?
+    a. WSD
+    b. needle thorakostomy
+    c. foto thoraks
+    d. pasang O2
+    e. pasang infus`,
+    correct_answer: "WSD"
+  },
+  {
+    desc: `Seorang laki-laki usia 48 tahun mengeluhkan keluar
+    benjolan pada anus. Benjolan awalnya bisa masuk
+    sendiri namun sekarang tidak bisa masuk kembali. Pada
+    pemeriksaan fisik didapatkan mukosa berwarna merah.
+    Diagnosis pasien ini?
+    a. Hemoroid interna grade II
+    b. Hemoroid interna grade III
+    c. Hemoroid interna grade IV
+    d. Prolapse recti
+    e. Fistula ani`,
+    correct_answer: "Hemoroid interna grade IV"
+  },
+  {
+    desc: `Laki 50 tahun datang dengan keluhan nyeri kepala hebat 1 jam yg
+    lalu disertai mual muntah. Pada 3 jam sebelumnya pasien
+    mengalami benturan pada kepala. Pasien sempat pingsan tapi
+    beberapa menit kemudian sadar. Kesadaran sopor, pupil
+    anisokor. Fenomena apakah yg terjadi pada kasus diatas?
+    a. Lucid interval
+    b. Komosio serebri
+    c. Amnesia
+    d. Demensia
+    e. Delerium`,
+    correct_answer: "Lucid interval"
+  },
+  {
+    desc: `Laki-laki 36 th, datang ke RS mengeluh sulit BAK. Setiap kali BAK
+    keluarnya hanya keluar menetes, harus mengedan, dan merasa
+    tidak puas. PF: CM, TD 120/80, HR 96 x/menit, RR 24 x/menit, S
+    36.7 C. Pemeriksaan RT didapatkan teraba prostat pada arah jam
+    12, pembesaran prostat pada arah jam 11 dan jam 1, konsistensi
+    kenyal, dan licin. Usulan pemeriksaan penunjang apa yg tepat?
+    a. CT scan
+    b. USG Abdomen
+    c. Foto Rontgen
+    d. USG Urologi
+    e. MRI`,
+    correct_answer: "USG Urologi"
+  },
+  {
+    desc: `Pria, 43 th dibawa ke IGD RS 3 jam setelah kecelakaan lalu lintas
+    dengan penurunan kesadaran dan terdapat jejas di perut bawah.
+    TD 70/palpasi, N 121x, RR 23x, suhu 37,5
+
+    oC. Ketika dilakukan
+    pemeriksaan radiologi dengan kontras, didapatkan ekstravasasi
+    ke lumen usus dan perirenal inferior. Diagnosis?
+    a. Ruptur buli derajat 1
+    b. Ruptur buli derajat 2
+    c. Ruptur buli derajat 3
+    d. Ruptur buli derajat 4
+    e. Ruptur buli derajat 5`,
+    correct_answer: "Ruptur buli derajat 5"
+  },
+  {
+    desc: `Perempuan berusia 34 tahun datang ke Puskesmas dengan keluhan
+    nyeri disekitar anus sejak 2 hari yang lalu. Pasien merasakan nyeri
+    perut bawah dan terasa BAB tidak tuntas. Riwayat berhubungan seks
+    anal 2 hari yang lalu. Pemeriksaan tanda vital dalam batas normal,
+    palpasi terdapat nyeri tekan, rectal toucher didapatkan ampula rekti
+    tidak kolaps, edema mukosa, nyeri tekan, lendir dan darah. Apakah
+    diagnosis yang paling mungkin?
+    a. Proktitis
+    b. Fisura ani
+    c. Hemoroid interna
+    d. Polip rekti
+    e. Fistula ani`,
+    correct_answer: "Proktitis"
+  },
+  {
+    desc: `Laki-laki usia 20 tahun ke IGD dengan mengeluh kesakitan
+    setelah mengalami kecelakaan lalu lintas. Kesadaran compos
+    mentis, TD: 138/80, N 99x, RR: 21x, Suhu 36,7
+
+    oC. Pada
+    pemeriksaan status lokalis didapatkan patah tulang terbuka
+    dengan perdarahan aktif. Tatalaksana awal pada kasus tersebut?
+    a. Bidai melewati dua sendi
+    b. Bidai diatas patah tulang
+    c. Bidai dibawah patah tulang
+    d. Bebat tekan
+    e. Reposisi dilanjutkan gips sirkular`,
+    correct_answer: "Bebat tekan"
+  },
+  {
+    desc: `Laki-laki 15 tahun datang ke IGD diantar oleh keluarganya karena
+    mengeluh nyeri pada lutut kanannya, hal tersebut terjadi akibat
+    jatuh setelah bermain basket. Pada pemeriksaan didapati
+    pergerakan sendi lutut terbatas, tampak bengkak, Mc Murrays
+    test (+) & krepitasi (-). Apa diagnosis pada pasien tersebut?
+    a. Fraktur femur distal
+    b. Meniscus tear
+    c. Tendon Achilles tear
+    d. Fraktur Tibia
+    e. Sprain ankle`,
+    correct_answer: "Meniscus tear"
+  },
+  {
+    desc: `Laki-laki, usia 34 tahun, datang dengan keluhan diare, BAB
+    berdarah, dan nyeri perut. Pada pemeriksaan fisik tekanan darah
+    110/70, nadi 80x/m, temp 37,5. Pada pemeriksaan kolonoskopi
+    dijumpai inflamasi kontinu mulai dari rektum sampai kolon
+    sigmoid. Pada histopatologi dijumpai distorsi crypt dan hiperemis
+    di lamina propia. Apa diagnosis pasien?
+    a. Kolitis ulseratif
+    b. Kolitis pseudomembra
+    c. Chron disease
+    d. Ileus obstruktif
+    e. Ca colon`,
+    correct_answer: "Kolitis ulseratif"
+  },
+  {
+    desc: `Seorang laki-laki, 54 th memiliki keluhan BAB berwarna pucat.
+    Pasien juga mengatakan lemas dan berat badan turun sebanyak
+    5kg dalam 2 bulan terakhir. Pada pemeriksaan fisik didapatkan
+    tanda vital dalam batas normal. Teraba massa pada epigastrium.
+    Tidak didapatkan pembesaran hepar. Pemeriksaan penunjang
+    apa yang dibutuhkan pada pasien ini?
+    a. CEA
+    b. PSA
+    c. CA 19-9
+    d. CA 125
+    e. SGOT/SGPT`,
+    correct_answer: "CA 19-9"
+  },
+  {
+    desc: `Laki-laki usia 60 tahun datang dengan keluhan nyeri seluruh bagian perut
+    yang disertai dengan mual muntah. Sebelumnya 2 hari yang lalu ia mengalami
+    nyeri ulu hati. Pasien diketahui memiliki riwayat minum obat anti nyeri untuk
+    lututnya. Dari pemeriksaan fisik ditemukan: keadaan umum pasien sangat
+    lemah, suhu 38,6OC dan defans muscular umum di seluruh lapang perut. Pada
+    pemeriksaan lab dan hasil yang didapatkan adalah: Hb 13g/dL, Leukosit 1700,
+    trombosit 355.000. Pada pemeriksaan radiologis ditemukan gambaran free air
+    abdomen pada sub diafragma kanan. Diagnosis yang paling mungkin pada
+    pasien tersebut adalah:
+    a. Peritonitis primer
+    b. Peritonitis tersier
+    c. Peritonitis appendix
+    d. Peritonitis sekunder e.c. perforasi appendix
+    e. Peritonitis sekunder e.c. perforasi lambung`,
+    correct_answer: "Peritonitis sekunder e.c. perforasi lambung"
+  },
+  {
+    desc: `Seorang laki laki berusia 60 tahun datang ke puskesmas dengan keluhan perut
+    terasa kembung selama 4 hari yang lalu. Riwayat benjolan pada lipat paha kiri
+    dirasakan sejak 1 tahun lalu. Awalnya benjolan menghilang apabila ia
+    berbaring namun dalam 5 hari terakhir benjolan menetap. Pemeriksaan tanda
+    vital diketahui TD 130/80 mmHg, HR 90 x, RR 24x, suhu 37,7
+
+    oC. Pemeriksaan
+    abdomen didapatkan perut tampak cembung, nyeri bila ditekan dan
+    peristaltik kesan meningkat. Apakah diagnosa yang paling mungkin?
+    a. Hernia inguinalis lateralis akreta
+    b. Hernia inguinalis lateralis skrotalis
+    c. Hernia inguinalis lateralis strangulata
+    d. Hernia inguinalis lateralis ireponibilis
+    e. Hernia inguinalis lateralis inkarserata`,
+    correct_answer: "Hernia inguinalis lateralis strangulata"
+  },
+  {
+    desc: `Seorang laki-laki berusia 20 tahun dibawa keluarganya ke UGD
+    dengan keluhan kesulitan BAK setelah terjatuh dari pohon.
+    pasien diketahui jatuh dengan terduduk. Dari pemeriksaan fisik
+    didapatkan adanya bloody discharge, butterfly hematom
+    perineal, dan retensi urin. apakah diagnosis yang paling tepat?
+    a. ruptur buli-buli
+    b. trauma penis
+    c. trauma skrotum
+    d. ruptur uretra
+    e. striktur uretra`,
+    correct_answer: "ruptur uretra"
+  },
+  {
+    desc: `Seorang laki-laki berusia 27 tahun dibawa ke IGD dengan keluhan nyeri perut
+    dan punggung setelah mengalami kecelakaan motor 2 jam sebelumnya.
+    Pemeriksaan tanda vital TD 70/palpasi, HR 120x/, GCS E4V5M6. Pemeriksaan
+    fisik didapatkan konjungtiva anemis, hematom pada punggung di flank kanan.
+    Berdasarkan keterangan saksi, pasien dibonceng, tertabrak motor lain dari
+    belakang, jatuh terbentur trotoar pada bagian belakang pasien. Apakah jenis
+    pemeriksaan penunjang yang tepat?
+    a. CT-Scan abdomen
+    b. MRI abdomen
+    c. Foto polos abdomen
+    d. FAST ( focused assessment sonography on trauma )
+    e. Pielografi intravena`,
+    correct_answer: "FAST ( focused assessment sonography on trauma )"
+  },
+  {
+    desc: `Seorang laki-laki usia 60 tahun mengeluhkan nyeri ulu hati yang
+    menjalar ke punggung dan perut sejak 8 hari yang lalu dan
+    memberat sejak 2 hari terakhir. Pasien memiliki riwayat
+    mengkonsumsi alkohol. TD 145/90, N 101x, RR 18x, Suhu 38oC.
+    Dari pemeriksaan penunjang lain, serum lipase 700, serta
+    terdapat gambaran ekimosis pada umbilikus. Diagnosis yang
+    tepat
+    a. Kolesistitis akut
+    b. Koledokolithiasis akut
+    c. Appendicitis akut
+    d. Pankreatitis akut
+    e. Hepatitis akut`,
+    correct_answer: "Pankreatitis akut"
+  },
+  {
+    desc: `Laki-laki usia 22 tahun datang dengan keluhan gangguan saat
+    berjalan sejak mengalami patah tulang 2 tahun yang lalu, saat itu
+    disarankan operasi namun pasien tidak mau dan memilih
+    berobat ke dukun. Didapatkan kaki tidak bisa dorsofleksi,
+    hipoestesi pada punggung kaki. Pemeriksaan rontgen didapatkan
+    fraktur pada fibula 1/3 proksimal. Keluhan yang dialami pasien
+    dikarenakan
+    a. Peroneal nerve palsy
+    b. Tibial nerve palsy
+    c. Malunion fracture
+    d. Sindrom kompartemen
+    e. Nonunion fracture`,
+    correct_answer: "Nonunion fracture"
+  },
+  {
+    desc: `Seorang laki-laki, usia 30 tahun datang
+    dengan keluhan sesak sejak 2 jam yang
+    lalu. Pasien riwayat kecelakaan
+    tertabrak stang motor. Pada
+    pemeriksaan fisik didapatkan TD :
+    90/70 mmhg, HR: 100x/m, RR: 40x/m.
+    deviasi trakea ke kiri, pada palpasi dada
+    kanan tertinggal, perkusi hipersonor,
+    auskultasi didapatkan suara nafas pada
+    dada kanan menghilang. Diagnosis ?
+    a. Pneumothorax
+    b. Hematothorax
+    c. Efusi pleura
+    d. Tamponade jantung
+    e. Tension pneumothorax`,
+    correct_answer: "Tension pneumothorax"
+  }
+]
